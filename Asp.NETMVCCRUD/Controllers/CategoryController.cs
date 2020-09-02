3e878940edcff00a2ad8ee270e2aaabcdff2d234
Maryam Iqbal <maryam_iqbal33@hotmail.com>
2020-09-02 23:18:27 +0200

    Added Category db, categorycontroller and category view

---
+++ Asp.NETMVCCRUD/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asp.NETMVCCRUD.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult AddorEdit(int id = 0)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddorEdit()
+        {
+            return View();
+        }
+    }
+}
